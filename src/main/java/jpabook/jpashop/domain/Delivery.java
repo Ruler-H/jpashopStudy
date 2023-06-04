240ae61f828c7ad2b20d0e62720d48cdacdd6db2
hruler <elwl5515@gmail.com>
2023-06-04 19:09:43 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @JsonIgnore
+    @OneToOne(fetch = LAZY, mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
